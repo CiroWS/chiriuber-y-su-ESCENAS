bcff21593a720657070b04beef51351fecf4350d
Rodimir909 <ignacioch909@gmail.com>
2026-06-05 22:27:34 -0300

    chiricelular

---
+++ CELULAR/CELULAR.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+
+func _on_CASINO_pressed():
+	get_tree().change_scene("res://CELULAR/CASINO/casinitopro.tscn")
+
+
+func _on_NAVECITA_pressed():
+	get_tree().change_scene("res://CELULAR/NAVECITAS/mundito pro.tscn")
